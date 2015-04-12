0c8c613c55d3d111e269af59d439e0ac2d21afa5
Timothy Miller <millerti@binghamton.edu>
2015-04-12 01:17:35 -0400

    Added much faster tanh implementation

--- gen_sse.rb
+++ gen_sse.rb
@@ -6,24 +6,20 @@ $double = false
 #$use_ssex = false
 $float = $double ? "double" : "float"
 $wsize = $double ? 2 : 1
-$quantize_fbits = 7
-$quantize_ibits = 0
 
 def quantize(len, ibits, fbits)
   x = ""
   x += "__attribute__((noinline)) static "
   x += "void quantize_#{len}_#{ibits}_#{fbits}(#{$float} *a) {\n"
-  x += "    int i, j, neg;\n"
+  x += "    int i, j;\n"
   x += "    #{$float} v;\n"
+  x += "    const #{$float} scale = 1.0 / #{(1<<(fbits-1))};\n"
   x += "    for (i=0; i<#{len}; i++) {\n"
   x += "        v = *a;\n"
-  x += "        neg = v < 0;\n"
-  x += "        if (neg) v = -v;\n"
-  x += "        j = v * #{(1<<fbits)} + 0.5;\n"
+  x += "        j = v * #{1<<(fbits-1)} + #{1<<(ibits+fbits-1)};\n"
+  x += "        if (j < 0) j = 0;\n"
   x += "        if (j > #{(1<<(ibits+fbits))-1}) j = #{(1<<(ibits+fbits))-1};\n"
-  #x += "        j &= #{(1<<(ibits+fbits))-1};\n"
-  x += "        v = (#{$float})j * (#{$float})#{1.0 / (1<<fbits)};\n"
-  x += "        if (neg) v = -v;\n"
+  x += "        v = (j - #{1<<(ibits+fbits-1)}) * scale;\n"
   x += "        *a++ = v;\n"
   x += "    }\n}\n\n"
   x
@@ -456,14 +452,15 @@ def mul_sig_prime(len)
   if $use_sse
     if $double
       if len >= 4
-        x += "    __m128d tgt, inp, one;\n"
-        x += "    one = _mm_set1_pd(1.0);\n"
+        x += "    __m128d tgt, inp;\n"
+        #x += "    __m128d tgt, inp, one;\n"
+        #x += "    one = _mm_set1_pd(1.0);\n"
         
         while (len >= 2)
           x += "    inp = _mm_load_pd(in+#{io});\n"
           x += "    tgt = _mm_load_pd(out+#{oo});\n"
-          x += "    inp = _mm_mul_pd(inp, inp);\n"
-          x += "    inp = _mm_sub_pd(one, inp);\n"
+          #x += "    inp = _mm_mul_pd(inp, inp);\n"
+          #x += "    inp = _mm_sub_pd(one, inp);\n"
           x += "    tgt = _mm_mul_pd(inp, tgt);\n"
           x += "    _mm_store_pd(out+#{oo}, tgt);\n"
 
@@ -474,14 +471,15 @@ def mul_sig_prime(len)
       end
     else
       if len >= 4
-        x += "    __m128 tgt, inp, one;\n"
-        x += "    one = _mm_set1_ps(1.0f);\n"
+        x += "    __m128 tgt, inp;\n"
+        #x += "    __m128 tgt, inp, one;\n"
+        #x += "    one = _mm_set1_ps(1.0f);\n"
         
         while (len >= 4)
           x += "    inp = _mm_load_ps(in+#{io});\n"
           x += "    tgt = _mm_load_ps(out+#{oo});\n"
-          x += "    inp = _mm_mul_ps(inp, inp);\n"
-          x += "    inp = _mm_sub_ps(one, inp);\n"
+          #x += "    inp = _mm_mul_ps(inp, inp);\n"
+          #x += "    inp = _mm_sub_ps(one, inp);\n"
           x += "    tgt = _mm_mul_ps(inp, tgt);\n"
           x += "    _mm_store_ps(out+#{oo}, tgt);\n"
 
@@ -495,11 +493,12 @@ def mul_sig_prime(len)
     
   while len > 0
     x += "    i = in[#{io}];\n"
-    if $double
-      x += "    out[#{oo}] *= 1.0 - i*i;\n"
-    else
-      x += "    out[#{oo}] *= 1.0f - i*i;\n"
-    end
+    x += "    out[#{oo}] *= i;\n"
+    #if $double
+      #x += "    out[#{oo}] *= 1.0 - i*i;\n"
+    #else
+      #x += "    out[#{oo}] *= 1.0f - i*i;\n"
+    #end
     io += 1
     oo += 1
     len -= 1
@@ -512,147 +511,107 @@ end
 
 # NOTE:  For only four, SSE is actually slower
 
-def sigmoid(len)
+def emit_tanh_float()
+  x =  "static float tanh_approx(float x) {\n"
+  x += "    union { unsigned int i; float f; } u, v;\n"
+  x += "    float a, b;\n"
+  x += "    const float scale = 16777216.0 / log(2.0);\n"
+  x += "    const float offset = 1065353216;\n"
+  x += "    u.i = x*scale + offset;\n"
+  x += "    v.i = -x*scale + offset;\n"
+  x += "    a = (u.f-1) / (u.f+1);\n"
+  x += "    b = -(v.f-1) / (v.f+1);\n"
+  x += "    return (a+b)*0.5f;\n"
+  x += "}\n\n"
+  x
+end
+
+def emit_tanh_double()
+  x =  "static double tanh_approx(double x) {\n"
+  x += "    union { unsigned long long i; double f; } u, v;\n"
+  x += "    double a, b;\n"
+  x += "    const double scale = 9007199254740992.0 / log(2.0);\n"
+  x += "    const double offset = 4607182418800017408.0;\n"
+  x += "    u.i = x*scale + offset;\n"
+  x += "    v.i = -x*scale + offset;\n"
+  x += "    a = (u.f-1) / (u.f+1);\n"
+  x += "    b = -(v.f-1) / (v.f+1);\n"
+  x += "    return (a+b)*0.5;\n"
+  x += "}\n\n"
+  x
+end
+
+
+
+def sigmoid(len, net)
   io = 0
-  x = "__attribute__((noinline)) void static sigmoid_#{len}(#{$float} *in) {\n"
-  x += "    #{$float} x, x2, af, bf;\n    int i;\n"
-    
-  # if $use_ssex
-  #   if $double
-  #     if len >= 8
-  #       x += "    __m128d F378, F17325, F135135, F28, F3150, F62370, Fsign, F4;\n"
-  #       x += "    __m128d inp, in2, a, b, abs, sign, gt4, one, ones;\n"
-  #       x += "    Fsign   = (__m128d)_mm_set1_epi64x(0x8000000000000000ULL);\n"
-  #       x += "    F4      = _mm_set1_pd(4.97178);\n"
-  #       x += "    F378    = _mm_set1_pd(378.0);\n"
-  #       x += "    F17325  = _mm_set1_pd(17325.0);\n"
-  #       x += "    F135135 = _mm_set1_pd(135135.0);\n"
-  #       x += "    F28     = _mm_set1_pd(28.0);\n"
-  #       x += "    F3150   = _mm_set1_pd(3150.0);\n"
-  #       x += "    F62370  = _mm_set1_pd(62370.0);\n"
-  #       x += "    one     = _mm_set1_pd(1.0);\n"
-  #
-  #       x += "    for (i=0; i<#{len / 2}; i++) {\n"
-  #       x += "        inp = _mm_load_pd(in);\n"
-  #       x += "        in2 = _mm_mul_pd(inp, inp);\n"
-  #
-  #       x += "        abs = _mm_andnot_pd(Fsign, inp);\n"
-  #       x += "        gt4 = _mm_cmpgt_pd(abs, F4);\n"
-  #
-  #       x += "        sign = _mm_and_pd(Fsign, inp);\n"
-  #       x += "        ones = _mm_or_pd(one, sign);\n"
-  #
-  #       x += "        a = _mm_add_pd(in2, F378);\n"
-  #       x += "        a = _mm_mul_pd(a, in2);\n"
-  #       x += "        a = _mm_add_pd(a, F17325);\n"
-  #       x += "        a = _mm_mul_pd(a, in2);\n"
-  #       x += "        a = _mm_add_pd(a, F135135);\n"
-  #       x += "        a = _mm_mul_pd(a, inp);\n"
-  #
-  #       x += "        b = _mm_mul_pd(in2, F28);\n"
-  #       x += "        b = _mm_add_pd(b, F3150);\n"
-  #       x += "        b = _mm_mul_pd(b, in2);\n"
-  #       x += "        b = _mm_add_pd(b, F62370);\n"
-  #       x += "        b = _mm_mul_pd(b, in2);\n"
-  #       x += "        b = _mm_add_pd(b, F135135);\n"
-  #
-  #       x += "        a = _mm_div_pd(a, b);\n"
-  #
-  #       x += "        a = _mm_andnot_pd(gt4, a);\n"
-  #       x += "        b = _mm_and_pd(gt4, ones);\n"
-  #       x += "        a = _mm_or_pd(a, b);\n"
-  #       x += "        _mm_store_pd(in, a);\n"
-  #
-  #       x += "        in += 2;\n"
-  #       x += "    }\n"
-  #
-  #       len &= 1
-  #     end
-  #   else
-  #     if len >= 8
-  #       x += "    __m128 F378, F17325, F135135, F28, F3150, F62370, Fsign, F4;\n"
-  #       x += "    __m128 inp, in2, a, b, abs, sign, gt4, one, ones;\n"
-  #       x += "    Fsign   = (__m128)_mm_set1_epi32(0x80000000);\n"
-  #       x += "    F4      = _mm_set1_ps(4.97178f);\n"
-  #       x += "    F378    = _mm_set1_ps(378.0f);\n"
-  #       x += "    F17325  = _mm_set1_ps(17325.0f);\n"
-  #       x += "    F135135 = _mm_set1_ps(135135.0f);\n"
-  #       x += "    F28     = _mm_set1_ps(28.0f);\n"
-  #       x += "    F3150   = _mm_set1_ps(3150.0f);\n"
-  #       x += "    F62370  = _mm_set1_ps(62370.0f);\n"
-  #       x += "    one     = _mm_set1_ps(1.0f);\n"
-  #
-  #       x += "    for (i=0; i<#{len / 4}; i++) {\n"
-  #       x += "        inp = _mm_load_ps(in);\n"
-  #       x += "        in2 = _mm_mul_ps(inp, inp);\n"
-  #
-  #       x += "        abs = _mm_andnot_ps(Fsign, inp);\n"
-  #       x += "        gt4 = _mm_cmpgt_ps(abs, F4);\n"
-  #
-  #       x += "        sign = _mm_and_ps(Fsign, inp);\n"
-  #       x += "        ones = _mm_or_ps(one, sign);\n"
-  #
-  #       x += "        a = _mm_add_ps(in2, F378);\n"
-  #       x += "        a = _mm_mul_ps(a, in2);\n"
-  #       x += "        a = _mm_add_ps(a, F17325);\n"
-  #       x += "        a = _mm_mul_ps(a, in2);\n"
-  #       x += "        a = _mm_add_ps(a, F135135);\n"
-  #       x += "        a = _mm_mul_ps(a, inp);\n"
-  #
-  #       x += "        b = _mm_mul_ps(in2, F28);\n"
-  #       x += "        b = _mm_add_ps(b, F3150);\n"
-  #       x += "        b = _mm_mul_ps(b, in2);\n"
-  #       x += "        b = _mm_add_ps(b, F62370);\n"
-  #       x += "        b = _mm_mul_ps(b, in2);\n"
-  #       x += "        b = _mm_add_ps(b, F135135);\n"
-  #
-  #       x += "        a = _mm_div_ps(a, b);\n"
-  #
-  #       x += "        a = _mm_andnot_ps(gt4, a);\n"
-  #       x += "        b = _mm_and_ps(gt4, ones);\n"
-  #       x += "        a = _mm_or_ps(a, b);\n"
-  #       x += "        _mm_store_ps(in, a);\n"
-  #
-  #       x += "        in += 4;\n"
-  #       x += "    }\n"
-  #
-  #       len &= 3
-  #     end
-  #   end
-  # end
-      
-  if (len > 1)
-    x += "    for (i=0; i<#{len}; i++) {\n"
-  end
+  x = "__attribute__((noinline)) static void sigmoid_#{len}(#{$float} *in, #{$float} *sigp) {\n"
+
   if $double
-    if (len > 0)
-      x += "        x = *in;\n"
-      x += "        x = tanh(x);\n"
-      # x += "        if (x < -4.97178) { x = -1.0; } else if (x > 4.97178) { x = 1.0; } else {\n"
-      # x += "            x2 = x * x;\n"
-      # x += "            af = x * (135135.0 + x2 * (17325.0 + x2 * (378.0 + x2)));\n"
-      # x += "            bf = 135135.0 + x2 * (62370.0 + x2 * (3150.0 + x2 * 28.0));\n"
-      # x += "            x = af / bf;\n"
-      # x += "        }\n"
-      x += "        *in++ = x;\n"
+    net.need_tanh = true
+    x += "    double v;\n"
+    if (len > 1)
+      x += "    int i;\n"
+      x += "    for (i=0; i<#{len}; i++) {\n"
+    end
+    x += "        v = *in;\n"
+    x += "        v = tanh_approx(v);\n"
+    x += "        *sigp = 1.0 - v*v;\n"
+    x += "        *in = v;\n"
+    if (len > 1)
+      x += "        sigp++; in++;\n"
+      x += "    }\n"
     end
   else
-    if (len > 0)
-      x += "        x = *in;\n"
-      x += "        x = tanhf(x);\n"
-      # x += "        if (x < -4.97178f) { x = -1.0f; } else if (x > 4.97178f) { x = 1.0f; } else {\n"
-      # x += "            x2 = x * x;\n"
-      # x += "            af = x * (135135.0f + x2 * (17325.0f + x2 * (378.0f + x2)));\n"
-      # x += "            bf = 135135.0f + x2 * (62370.0f + x2 * (3150.0f + x2 * 28.0f));\n"
-      # x += "            x = af / bf;\n"
-      # x += "        }\n"
-      x += "        *in++ = x;\n"
+    if $use_sse && (len>1)
+      x += "    __m128 Fzero = _mm_setzero_ps();\n"
+      x += "    const float Dscale = 16777216.0 / log(2.0);\n"
+      x += "    __m128 Fscale = _mm_set1_ps(Dscale);\n"
+      x += "    __m128 Foffset = _mm_set1_ps(1065353216.0);\n"
+      x += "    __m128 Fone = _mm_set1_ps(1.0);\n"
+      x += "    __m128 Fhalf = _mm_set1_ps(0.5);\n"
+      x += "    __m128 x, u, um1, up1, v, vm1, vp1, a, b;\n"
+      loops = (len+3)/4
+      if (loops > 1)
+        x += "    int i;\n"
+        x += "    for (i=0; i<#{loops}; i++) {\n"
+      end
+      x += "        x = _mm_load_ps(in);\n"
+      x += "        u = _mm_add_ps(_mm_mul_ps(x, Fscale), Foffset);\n"
+      x += "        u = (__m128)_mm_cvttps_epi32(u);\n"
+      x += "        x = _mm_sub_ps(Fzero, x);\n"
+      x += "        v = _mm_add_ps(_mm_mul_ps(x, Fscale), Foffset);\n"
+      x += "        v = (__m128)_mm_cvttps_epi32(v);\n"
+      x += "        a = _mm_div_ps(_mm_sub_ps(u, Fone), _mm_add_ps(u, Fone));\n"
+      x += "        b = _mm_div_ps(_mm_sub_ps(v, Fone), _mm_add_ps(v, Fone));\n"
+      x += "        b = _mm_sub_ps(Fzero, b);\n"
+      x += "        a = _mm_mul_ps(_mm_add_ps(a, b), Fhalf);\n"
+      x += "        b = _mm_sub_ps(Fone, _mm_mul_ps(a, a));\n"
+      x += "        _mm_store_ps(in, a);\n"
+      x += "        _mm_store_ps(sigp, b);\n"
+      if (loops > 1)
+        x += "        in += 4;\n"
+        x += "        sigp += 4;\n"
+        x += "    }\n"
+      end
+    else
+      net.need_tanh = true
+      x += "    float v;\n"
+      if (len > 1)
+        x += "    int i;\n"
+        x += "    for (i=0; i<#{len}; i++) {\n"
+      end
+      x += "        v = *in;\n"
+      x += "        v = tanh_approx(v);\n"
+      x += "        *sigp = 1.0f - v*v;\n"
+      x += "        *in = v;\n"
+      if (len > 1)
+        x += "       sigp++; in++;\n"
+        x += "    }\n"
+      end
     end
   end
-  if (len > 1)
-    x += "    }\n"
-  end
-    
+  
   x + "}\n\n"
 end
 
@@ -705,9 +664,9 @@ end
 
 class Layer
   # Inputs
-  attr_accessor :n_in, :in_val, :in_del
+  attr_accessor :n_in, :in_val, :in_del, :in_sig_prime
   # Outputs
-  attr_accessor :n_out, :out_val, :out_del, :sig
+  attr_accessor :n_out, :out_val, :out_del, :sig, :val_offset, :out_sig_prime
   # Weights
   attr_accessor :weights
   attr_accessor :mynum
@@ -720,9 +679,10 @@ class Layer
 end
 
 class LayerSpec
-  attr_accessor :size, :quantize, :quanti, :quantf, :sigmoid
+  attr_accessor :size, :quantize, :quanti, :quantf, :sigmoid, :input
 
-  def initialize(word)
+  def initialize(word, input)
+    @input = input
     @quantize = false
     @sigmoid = true
     @quanti = 0
@@ -743,6 +703,7 @@ class LayerSpec
     end
 
     if word.size > 0 && word[0] == 'q'
+      word.shift
       @quantize = true
       i_s = ""
       f_s = ""
@@ -762,10 +723,12 @@ class LayerSpec
 
   def comment
     x = "#{@size} nodes"
-    if @sigmoid
-      x += ", tanh activation"
-    else
-      x += ", linear activation"
+    if !@input
+      if @sigmoid
+        x += ", tanh activation"
+      else
+        x += ", linear activation"
+      end
     end
     if @quantize
       x += ", quantized to #{@quanti}.#{@quantf}"
@@ -774,7 +737,11 @@ class LayerSpec
   end
 
   def to_s
-    "#{@size}#{@sigmoid ? 's' : 'l'}#{@quantize ? "q" : ""}"
+    if @input
+      "#{size}"
+    else
+      "#{@size}#{@sigmoid ? 's' : 'l'}#{@quantize ? "q" : ""}"
+    end
   end
 end
 
@@ -785,7 +752,7 @@ class Network
 
   # Pending needs
   attr_accessor :need_dotprod, :need_sum_scaled, :need_mul_sig_prime
-  attr_accessor :need_subtract, :need_subtract_sig, :need_sigmoid
+  attr_accessor :need_subtract, :need_subtract_sig, :need_sigmoid, :need_tanh
   attr_accessor :need_copy, :need_clear
 
   # Functions
@@ -799,13 +766,13 @@ class Network
     end
     x += "static void randomize_#{size}(#{$float} *mem) {\n"
     x += "    const double RMI = 1.0 / RAND_MAX;\n"
-    x += "    double b2 = pow((double)#{size}, -0.5) * sqrt(12.0);\n"
-    x += "    double b = b2*0.5;\n"
-    x += "    b2 *= RMI;\n"
+    x += "    const double b2 = pow((double)#{size}, -0.5) * sqrt(12.0);\n"
+    x += "    const double b = b2*0.5;\n"
+    x += "    const double b3 = b2*RMI;\n"
     x += "    int i;\n"
     x += "    for (i=0; i<=#{size}; i++) {\n"
     x += "        do {\n"
-    x += "            mem[i] = random() * b2 - b;\n"
+    x += "            mem[i] = random() * b3 - b;\n"
     x += "        } while (mem[i] == 0);\n"
     x += "    }\n"
     x += "}\n\n"
@@ -848,6 +815,7 @@ class Network
     @need_copy = []
     @need_clear = []
     @need_quantize = []
+    @need_tanh = false
         
     @defines = allocate(layer_list)
     @out_tmp = "(mem+#{allocate_block(layer_list[-1].size)})"
@@ -869,7 +837,18 @@ class Network
     @funcs = @allocator
     @funcs += randomize #(@mem_size, @name)
     @need_quantize.each { |i| @funcs += quantize(i[0], i[1], i[2]); }
-    @need_sigmoid.each { |i| @funcs += sigmoid(i); }
+
+    sigfuncs = ""
+    @need_sigmoid.each { |i| sigfuncs += sigmoid(i, self); }
+    if @need_tanh
+      if $double
+        @funcs += emit_tanh_double()
+      else
+        @funcs += emit_tanh_float()
+      end
+    end
+    @funcs += sigfuncs
+
     @need_dotprod.each { |i| @funcs += dotprod(i); }
     @need_sum_scaled.each { |i| @funcs += sum_scaled(i); }
     @need_mul_sig_prime.each { |i| @funcs += mul_sig_prime(i); }
@@ -883,12 +862,13 @@ class Network
       @code += "#include <pmmintrin.h>\n"
     end
     @code += "#include <math.h>\n#include <stdlib.h>\n#include <stdio.h>\n#include <string.h>\n\n"
-    @code += @defines + @funcs + @fwd + @bkw
+    @code += @defines + @funcs + @fwd + @bkw + layer_vals
     @code += "\n#else /* HEADER FOLLOWS */\n\n"
     @code += allocate_func_h(@mem_size, @name)
     @code += forward_h
     @code += backward_h
     @code += randomize_h(@mem_size, @name)
+    @code += layer_vals_h
     @code += "\n#endif\n"
   end
 
@@ -919,16 +899,23 @@ class Network
     @in_tmp = "IN_TMP"
     prev_val = "IN_TMP"
     prev_del = nil
+    prev_sig_prime = nil
         
     ln = 1
             
     while (l.size > 0)
       layerspec = l.shift
       nnodes = layerspec.size
+
       values = allocate_block(nnodes)
       val_ptr = "(mem+#{values})"
       x += "#define L#{ln}_VAL #{val_ptr}\n"
       val_ptr = "L#{ln}_VAL"
+
+      sigp = allocate_block(nnodes);
+      sigp_ptr = "(mem+#{sigp})"
+      x += "#define L#{ln}_SIGP #{sigp_ptr}\n"
+      sigp_ptr = "L#{ln}_SIGP"
             
       deltas = allocate_block(nnodes)
       del_ptr = "(mem+#{deltas})"
@@ -939,14 +926,17 @@ class Network
       layer.n_in = prev_nnodes
       layer.n_out = nnodes
       layer.in_val = prev_val
+      layer.in_sig_prime = prev_sig_prime
       layer.out_val = val_ptr
+      layer.out_sig_prime = sigp_ptr
+      layer.val_offset = values
       layer.in_del = prev_del
       layer.out_del = del_ptr
       layer.quantize = layerspec.quantize
       layer.quanti = layerspec.quanti
       layer.quantf = layerspec.quantf
       layer.sig = layerspec.sigmoid
-      layer.mynum = @layers.size
+      layer.mynum = ln
             
       for i in 0...nnodes do
         weights = allocate_block(prev_nnodes + 1)
@@ -959,11 +949,25 @@ class Network
             
       prev_nnodes = nnodes
       prev_val = val_ptr
+      prev_sig_prime = sigp_ptr
       prev_del = del_ptr
             
       ln += 1
     end
         
+    x
+  end
+
+  def layer_vals_h
+    x = "extern int layer_values_#{@name}[#{layers.size+1}];\n"
+  end
+
+  def layer_vals
+    x = "int layer_values_#{@name}[#{layers.size+1}] = {0"
+    layers.each do |layer|
+      x += ", #{layer.val_offset}"
+    end
+    x += "};\n"
     x
   end
 
@@ -974,7 +978,7 @@ class Network
     end
     x + "#{$float} *forward_#{@name}(#{$float} *in, #{$float} *mem);\n"
   end
-    
+
   def forward
     @need_copy << layers.first.n_in
     x = ""
@@ -986,6 +990,7 @@ class Network
       nnodes = layer.n_out
       prev_nnodes = layer.n_in
       ptr = layer.out_val
+      sigp = layer.out_sig_prime
             
       @need_dotprod << prev_nnodes
             
@@ -998,7 +1003,7 @@ class Network
             
       if (layer.sig)
         @need_sigmoid << nnodes
-        x += "    sigmoid_#{nnodes}(#{ptr});\n"
+        x += "    sigmoid_#{nnodes}(#{ptr}, #{sigp});\n"
       end
 
       if (layer.quantize)
@@ -1066,8 +1071,10 @@ class Network
         x += "    sum_scaled_#{l.n_in}(#{l.weights[i]}, #{l.in_del}, "
         x += "*(#{l.out_del}+#{i}));\n"
       end
+# XXX Oops.  Assumes that every non-output layer is tanh!
       @need_mul_sig_prime << l.n_in
-      x += "    mul_sig_prime_#{l.n_in}(#{l.in_val}, #{l.in_del});\n"
+      #x += "    mul_sig_prime_#{l.n_in}(#{l.in_val}, #{l.in_del});\n"
+      x += "    mul_sig_prime_#{l.n_in}(#{l.in_sig_prime}, #{l.in_del});\n"
     end
         
     # Loop over layers, adjusting weights
@@ -1082,9 +1089,9 @@ class Network
         # if (i == 1 && l.mynum == 0)
         #   x += "printf(\"%f\\n\", odel);\n"
         # end
+        x += "    *(#{l.weights[i]}+#{l.n_in}) += odel;"
         x += "    sum_scaled_#{l.n_in}(#{inputs}, "
-        x += "#{l.weights[i]}, odel);"
-        x += "    *(#{l.weights[i]}+#{l.n_in}) += odel;\n"
+        x += "#{l.weights[i]}, odel);\n"
       end
       inputs = l.out_val
     end
@@ -1095,8 +1102,10 @@ end
 
 layers = []
 
+is_input = true
 ARGV.each do |arg|
-  layers << LayerSpec.new(arg)
+  layers << LayerSpec.new(arg, is_input)
+  is_input = false
 end
 
 layers.each_index do |i|
